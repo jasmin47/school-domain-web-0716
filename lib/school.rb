ff4c1c908871b4477c1fd6f0dc7d96399c5aba2c
jasmin47 <alxsanborn@gmail.com>
2016-07-27 09:04:14 -0400

    solved

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School
+
+  attr_reader :roster
+
+  def initialize(roster)
+    roster = {}
+    @roster = roster
+  end
+
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else @roster[grade] = [student]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+Hash[@roster.sort]
+
+@roster.each {|key, value_array|
+value_array.sort!
+}
+
+  end
+
+
+  end
+
+
